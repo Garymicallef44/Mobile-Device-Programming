48237228177bbb1a84fe8ae74d9963395e972a7f
David Isaac Ohayon <147913714+davidisaacOhayon@users.noreply.github.com>
2025-11-26 11:46:31 +0100

    Details Update

--- app/(tabs)/garage.tsx
+++ app/(tabs)/garage.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Pressable, ScrollView, Text, TextInput, View } from "react-native";
+import { Pressable, Text, TextInput, View } from "react-native";
 
 import Navbar from "@/components/navbar";
 import { saveUserCarDetails, UserCarDetails } from "../backend/AsyncStorage";
@@ -25,200 +25,206 @@ export default function CarDetails() {
   );
 
   return (
-    <ScrollView
-      style={{ flex: 1, backgroundColor: "#fff" }}
-      contentContainerStyle={{ padding: 20, paddingTop: 40, paddingBottom: 60 }}
-    >
-      {/* Top Bar */}
-      <Navbar />
-
-      {/* Title */}
-      <Text
-        style={{
-          fontSize: 34,
-          fontWeight: "700",
-          marginBottom: 10,
-          lineHeight: 38,
-        }}
-      >
-        Enter your{"\n"}car details
-      </Text>
-
-      {/* Car Model & Make */}
-<View style={{ marginTop: 20 }}>
-  <Text style={{ fontWeight: "700", marginBottom: 6 }}>Car Model & Make</Text>
-
-  <TextInput
-    placeholder="e.g. Toyota Corolla"
-    style={{
-      backgroundColor: "#f3f3f3",
-      borderRadius: 10,
-      paddingHorizontal: 12,
-      paddingVertical: 10,
-    }}
-    onChangeText={(setCarModel)}
-  />
-
-  {/* Engine Type label under input */}
-  <Text style={{ fontWeight: "700", marginTop: 12, marginBottom: 6 }}>
-    Engine type
-  </Text>
-
-  {/* Engine Toggle */}
-  <View
-    style={{
-      flexDirection: "row",
-      backgroundColor: "#ddd",
-      borderRadius: 20,
-      padding: 3,
-      alignSelf: "flex-start",
-    }}
-  >
-    {["Electric", "Gas"].map((type) => {
-      const selected = engineType === type;
-      return (
-        <Pressable
-          key={type}
-          onPress={() => {
-            if (type == "Electric"){
-              setFuelType(null);
-            }
-            setEngineType(type as any)
-
-          }
-          }
-          style={{
-            paddingVertical: 6,
-            paddingHorizontal: 14,
-            borderRadius: 20,
-            backgroundColor: selected ? "#345CFF" : "transparent",
-          }}
-        >
-          <Text
-            style={{
-              color: selected ? "#fff" : "#555",
-              fontWeight: "600",
-            }}
-          >
-            {type}
-          </Text>
-        </Pressable>
-      );
-    })}
-  </View>
-</View>
-
-
-{/* VIN Number */}
-<View style={{ marginTop: 25 }}>
-  <Text style={{ fontWeight: "700", marginBottom: 6 }}>Vin Number</Text>
-
-  <TextInput
-    placeholder="e.g. Toyota Corolla"
-    style={{
-      backgroundColor: "#f3f3f3",
-      borderRadius: 10,
-      paddingHorizontal: 12,
-      paddingVertical: 10,
-    }}
-    onChangeText={setVinNo}
-  />
-
-  {/* Fuel Type ONLY when engine is Gas */}
-  {engineType === "Gas" && (
     <>
-      <Text style={{ fontWeight: "700", marginTop: 12, marginBottom: 6 }}>
-        Fuel Type
-      </Text>
 
       <View
-        style={{
-          flexDirection: "row",
-          backgroundColor: "#ddd",
-          borderRadius: 20,
-          padding: 3,
-          alignSelf: "flex-start",
-        }}
+        style={{ flex: 1, backgroundColor: "#fff" }}
+        contentContainerStyle={{paddingBottom: 60 }}
       >
-        {["Petrol", "Diesel"].map((type) => {
-          const selected = fuelType === type;
-          return (
-            <Pressable
-              key={type}
-              onPress={() => setFuelType(type as any)}
+      <Navbar />
+        {/* Top Bar */}
+
+      <View style={{padding: 20}}>
+
+        {/* Title */}
+        <Text
+          style={{
+            fontSize: 34,
+            fontWeight: "700",
+            marginBottom: 10,
+            lineHeight: 38,
+          }}
+        >
+          Enter your{"\n"}car details
+        </Text>
+
+        {/* Car Model & Make */}
+    <View style={{ marginTop: 20 }}>
+    <Text style={{ fontWeight: "700", marginBottom: 6 }}>Car Model & Make</Text>
+
+    <TextInput
+      placeholder="e.g. Toyota Corolla"
+      style={{
+        backgroundColor: "#f3f3f3",
+        borderRadius: 10,
+        paddingHorizontal: 12,
+        paddingVertical: 10,
+      }}
+      onChangeText={(setCarModel)}
+    />
+
+    {/* Engine Type label under input */}
+    <Text style={{ fontWeight: "700", marginTop: 12, marginBottom: 6 }}>
+      Engine type
+    </Text>
+
+    {/* Engine Toggle */}
+    <View
+      style={{
+        flexDirection: "row",
+        backgroundColor: "#ddd",
+        borderRadius: 20,
+        padding: 3,
+        alignSelf: "flex-start",
+      }}
+    >
+      {["Electric", "Gas"].map((type) => {
+        const selected = engineType === type;
+        return (
+          <Pressable
+            key={type}
+            onPress={() => {
+              if (type == "Electric"){
+                setFuelType(null);
+              }
+              setEngineType(type as any)
+
+            }
+            }
+            style={{
+              paddingVertical: 6,
+              paddingHorizontal: 14,
+              borderRadius: 20,
+              backgroundColor: selected ? "#345CFF" : "transparent",
+            }}
+          >
+            <Text
               style={{
-                paddingVertical: 6,
-                paddingHorizontal: 14,
-                borderRadius: 20,
-                backgroundColor: selected ? "#4CAF50" : "transparent",
+                color: selected ? "#fff" : "#555",
+                fontWeight: "600",
               }}
             >
-              <Text
+              {type}
+            </Text>
+          </Pressable>
+        );
+      })}
+    </View>
+    </View>
+
+
+    {/* VIN Number */}
+    <View style={{ marginTop: 25 }}>
+    <Text style={{ fontWeight: "700", marginBottom: 6 }}>Vin Number</Text>
+
+    <TextInput
+      placeholder="e.g. Toyota Corolla"
+      style={{
+        backgroundColor: "#f3f3f3",
+        borderRadius: 10,
+        paddingHorizontal: 12,
+        paddingVertical: 10,
+      }}
+      onChangeText={setVinNo}
+    />
+
+    {/* Fuel Type ONLY when engine is Gas */}
+    {engineType === "Gas" && (
+      <>
+        <Text style={{ fontWeight: "700", marginTop: 12, marginBottom: 6 }}>
+          Fuel Type
+        </Text>
+
+        <View
+          style={{
+            flexDirection: "row",
+            backgroundColor: "#ddd",
+            borderRadius: 20,
+            padding: 3,
+            alignSelf: "flex-start",
+          }}
+        >
+          {["Petrol", "Diesel"].map((type) => {
+            const selected = fuelType === type;
+            return (
+              <Pressable
+                key={type}
+                onPress={() => setFuelType(type as any)}
                 style={{
-                  color: selected ? "#fff" : "#555",
-                  fontWeight: "600",
+                  paddingVertical: 6,
+                  paddingHorizontal: 14,
+                  borderRadius: 20,
+                  backgroundColor: selected ? "#4CAF50" : "transparent",
                 }}
               >
-                {type}
-              </Text>
-            </Pressable>
-          );
-        })}
+                <Text
+                  style={{
+                    color: selected ? "#fff" : "#555",
+                    fontWeight: "600",
+                  }}
+                >
+                  {type}
+                </Text>
+              </Pressable>
+            );
+          })}
+        </View>
+      </>
+    )}
+    </View>
+
+
+        {/* Extra Specifics */}
+        <View style={{ marginTop: 40 }}>
+          <Text style={{ fontWeight: "700", marginBottom: 6 }}>
+            Extra Specifics
+          </Text>
+          <TextInput
+            multiline
+            placeholder="Input any extra details that may be important for service garages."
+            style={{
+              backgroundColor: "#f3f3f3",
+              borderRadius: 12,
+              padding: 12,
+              minHeight: 130,
+              textAlignVertical: "top",
+            }}
+            onChangeText={setExtraDetail}
+          />
+        </View>
+
+        {/* Save Button */}
+        <Pressable
+          style={{
+            backgroundColor: "#FFB869",
+            paddingVertical: 16,
+            alignSelf: "center",
+            marginTop: 20,
+            borderRadius: 20,
+            paddingHorizontal: 40,
+          }}
+          onPress={() => {
+
+            if (!carModel?.trim() || !vinNo?.trim() || !extraDetail?.trim()){
+              alert("Please fill in all details.")
+              return;
+            }
+            const body: UserCarDetails = {
+              model: carModel!,
+              vNumber: vinNo!,
+              detail: extraDetail!,
+              fuelType: fuelType!,
+              engineType: engineType! 
+            }
+
+            saveUserCarDetails(body);
+
+          }}
+        >
+          <Text style={{ fontWeight: "700", fontSize: 16 }}>Save Vehicle</Text>
+        </Pressable>
+        </View>
       </View>
     </>
-  )}
-</View>
-
-
-      {/* Extra Specifics */}
-      <View style={{ marginTop: 40 }}>
-        <Text style={{ fontWeight: "700", marginBottom: 6 }}>
-          Extra Specifics
-        </Text>
-        <TextInput
-          multiline
-          placeholder="Input any extra details that may be important for service garages."
-          style={{
-            backgroundColor: "#f3f3f3",
-            borderRadius: 12,
-            padding: 12,
-            minHeight: 130,
-            textAlignVertical: "top",
-          }}
-          onChangeText={setExtraDetail}
-        />
-      </View>
-
-      {/* Save Button */}
-      <Pressable
-        style={{
-          backgroundColor: "#FFB869",
-          paddingVertical: 16,
-          alignSelf: "center",
-          marginTop: 50,
-          borderRadius: 20,
-          paddingHorizontal: 40,
-        }}
-        onPress={() => {
-
-          if (!carModel?.trim() || !vinNo?.trim() || !extraDetail?.trim()){
-            alert("Please fill in all details.")
-            return;
-          }
-          const body: UserCarDetails = {
-            model: carModel!,
-            vNumber: vinNo!,
-            detail: extraDetail!,
-            fuelType: fuelType!,
-            engineType: engineType! 
-          }
-
-          saveUserCarDetails(body);
-
-        }}
-      >
-        <Text style={{ fontWeight: "700", fontSize: 16 }}>Save Vehicle</Text>
-      </Pressable>
-    </ScrollView>
   );
 }
