4fd2120a0c1fc897707c4140081e06f69c6d41dc
Keet <k8schem@gmail.com>
2026-01-03 21:38:50 +0100

    Added a the program that sends notifications

---
+++ backend/notif-extern.ts
@@ -0,0 +1,21 @@
+
+const sendNotif = async (id:string,title:string,message:string)=>{
+  try{
+    const response = await fetch("http://localhost:3000/send-notif",{
+      method:"POST",
+      headers:{"Content-Type": "application/json"},
+      body: JSON.stringify({
+        id:id,
+        title:title,
+        msg:message,
+        
+      }),
+    });
+    console.log(response);
+    const data= await response.json();
+  }catch(err){
+    console.error("Error sending notification:",err);
+  }
+};
+
+sendNotif('YOUR ID HERE','Hello','This is a Test Notification');
