ebb5eac0f9cc9825c673d0a29cf0593ad35d8047
Keet <k8schem@gmail.com>
2026-01-06 23:30:29 +0100

    Update notif-extern.ts

--- backend/notif-extern.ts
+++ backend/notif-extern.ts
@@ -1,7 +1,7 @@
 
 export const sendNotif = async (id:string,title:string,message:string)=>{
   try{
-    const response = await fetch("http://localhost:3000/send-notif",{
+    const response = await fetch("http://10.0.2.2:3000/send-notif",{
       method:"POST",
       headers:{"Content-Type": "application/json"},
       body: JSON.stringify({
